d7c67d23a11b43c92d3058693c5128be089cbf74
Thales Zika de Oliveira <thaleslol2003@gmail.com>
2025-05-04 17:16:07 -0300

    Projeto Angular FrotaPro

---
+++ src/app/pages/motorista/homepage-motorista/homepage-motorista.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage-motorista',
+  imports: [],
+  templateUrl: './homepage-motorista.component.html',
+  styleUrl: './homepage-motorista.component.css'
+})
+export class HomepageMotoristaComponent {
+
+}
