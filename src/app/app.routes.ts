b9d9f2d4e00cc5fcdf1a016a75e809528355a1a7
Vinicius <vinicius@radek.com.br>
2025-05-20 19:35:27 -0300

    tela do motora

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { HomepageMotoristaComponent } from './pages/motorista/homepage-motorista';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
+    { path: 'motorista', component: HomepageMotoristaComponent},
     { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
