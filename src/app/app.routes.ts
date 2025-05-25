2840e8b6888190d4ce235ccd06fa222283aa4a99
Ariane Souza <ari.souza02@outlook.com>
2025-05-25 18:36:20 -0300

    chore: criado pasta pra englobar front e back

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { HomepageMotoristaComponent } from './pages/motorista/homepage-motorista';
-
-export const routes: Routes = [
-    { path: 'login', component: LoginComponent },
-    { path: 'motorista', component: HomepageMotoristaComponent},
-    { path: '', redirectTo: 'login', pathMatch: 'full' }
-];
